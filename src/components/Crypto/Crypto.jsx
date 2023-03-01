92ab9b3e437d207b44647320e051645c86f3f069
Oscar Harron <oscarharron@hotmail.com>
2023-03-01 16:43:19 +1300

    add working btc line chart

--- src/components/Crypto/Crypto.jsx
+++ src/components/Crypto/Crypto.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Crypto.module.css';
 import { Inter } from '@next/font/google';
 
+import Graph from './Graph';
 const inter = Inter({ subsets: ['latin'] });
 
 async function getData() {
@@ -22,13 +23,31 @@ async function getData() {
   return top2;
 }
 
+async function getDailyChange() {
+  const res = await fetch(
+    'https://api.coincap.io/v2/assets/bitcoin/history?interval=h1'
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const { data } = await res.json();
+  const history = data.slice(0, 24).map((d) => ({
+    price: Number(d.priceUsd),
+  }));
+  return history;
+}
+
 export default async function Crypto() {
   const data = await getData();
+  const btcChange = await getDailyChange();
+  console.log(btcChange.length);
 
   return (
     <div className={inter.className}>
       <span className={styles.tag}>Crypto Tracker</span>
       <div>
+        <Graph change={btcChange} />
         {data.map((cryp) => (
           <div key={cryp.rank}>
             <h3>
