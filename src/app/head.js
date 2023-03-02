c9c9978c8135f01f71bf6a0482f0cedeb889f7b2
Oscar Harron <oscarharron@hotmail.com>
2023-03-02 17:13:29 +1300

    add mobile styling

--- src/app/head.js
+++ src/app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Dashboard</title>
+      <title>Dashboard | Oscar</title>
       <meta
         content="width=device-width, initial-scale=1"
         name="viewport"
